c0976d1e747b9807c84f96f57d7b295e2e8efbe7
Everton <everton.aga88@gmail.com>
2023-05-20 15:22:59 +0100

    Update settings.py

--- ondacertaweb/settings.py
+++ ondacertaweb/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
 CSRF_TRUSTED_ORIGINS = ['https://ondacerta.up.railway.app'] ##verificar isso rpa deploy
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_SSL_REDIRECT = True
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
