29129b5e3d7a6b1ca508a44ec977650ac83aeb06
Everton <everton.aga88@gmail.com>
2023-05-20 20:52:25 +0100

    Update urls.py

--- ondacertaweb/urls.py
+++ ondacertaweb/urls.py
@@ -1,14 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ondacertaradio.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('ondacertaradio.urls')),
+    re_path(r'^media/(?P<path>.*)$',serve, {'document_root': settings.MEDIA_ROOT}),
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 admin.AdminSite.site_header = "Onda Certa Web Radio"
