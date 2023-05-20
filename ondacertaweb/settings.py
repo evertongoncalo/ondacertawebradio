0c2f0adf937b181a6b2c9066ae412bf3b677530c
Everton <everton.aga88@gmail.com>
2023-05-20 12:22:19 +0100

    deded

--- ondacertaweb/settings.py
+++ ondacertaweb/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wqs_(xf8#plgn&#+rwy4947jt(7(*r=yc!2nv^zva51#%21j84'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,3 +123,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
