ddedfa81788c16d8eef1d35cf297304bf2f3f696
Everton <everton.aga88@gmail.com>
2023-07-08 04:48:20 +0100

    try

--- staticfiles/assets/js/player.js
+++ staticfiles/assets/js/player.js
@@ -1,19 +1,27 @@
-async function radio(){
+async function radio() {
   try {
-    const play = await fetch('https://freeuk21.listen2myradio.com/live.mp3?typeportmount=s1_28364_stream')
-    return play
-  } catch (error) {
-    console.log(error)
+    const response = await fetch('https://freeuk21.listen2myradio.com/live.mp3?typeportmount=s1_28364_stream');
+    if (!response.ok) {
+      throw new Error('Erro ao obter o áudio');
+    }
+    const audio = await response.blob();
+    const audioURL = URL.createObjectURL(audio);
     
+    // Exemplo de reprodução do áudio usando um elemento de áudio HTML5
+    const audioElement = document.createElement('audio');
+    audioElement.src = audioURL;
+    audioElement.controls = true;
+    document.body.appendChild(audioElement);
+    
+    return audioElement;
+  } catch (error) {
+    console.log(error);
   }
-  
- 
-  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     $('#play-pause-button').click(function() {
-      var audio = $('https://freeuk21.listen2myradio.com/live.mp3?typeportmount=s1_28364_stream').get(0);
+      var audio = $('#sound').get(0);
       $(this).toggleClass('active');
   
       if ($(this).hasClass('active')) {
@@ -26,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   
     $('#play-pause-button2').click(function() {
-      var audio = $('https://freeuk21.listen2myradio.com/live.mp3?typeportmount=s1_28364_stream').get(0);
+      var audio = $('#sound').get(0);
       $(this).toggleClass('active');
   
       if ($(this).hasClass('active')) {
@@ -39,3 +47,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }); 
   
+
+  document.addEventListener('DOMContentLoaded', radio);
